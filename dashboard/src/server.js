6c139efa4ff9649d7dfdab9e32bfded27a756497
Victor Norgren <victor@logotype.se>
2018-10-14 22:19:21 +0800

    dashboard-server

---
+++ dashboard/src/server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+app.use(express.static('build'));
+app.listen(port, () => console.log(`FIXParserDashboard listening on port ${port}.`));
